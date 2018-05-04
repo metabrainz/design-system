646f283915e0c487c948e74d87176fe21f2341ce
chhavi <s.chhavi@iitg.ac.in>
2018-05-04 19:17:58 +0530

    Add a test button story

---
+++ components/TestButton.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+import {Button} from 'react-bootstrap';
+
+/* I think it will need to converted to css first*/
+import '../less/buttons.less';
+import '../less/colors.less';
+
+/* Write some react function here?*/
+
+function TestButton(){
+	return(
+		<button class="btn" type="button">Default button</button>
+			);
+
+}
+
+export default TestButton;
+
---
+++ stories/index.stories.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import { storiesOf } from '@storybook/react';
+import { action } from '@storybook/addon-actions';
+import { linkTo } from '@storybook/addon-links';
+
+import { Button, Welcome } from '@storybook/react/demo';
+
+import TestButton from '../components/TestButton.js';
+
+storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+storiesOf('TestButton', module)
+  .add('TestButton', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
+      <span role="img" aria-label="so cool">
+        😀 😎 👍 💯
+      </span>
+    </Button>
+  ));
