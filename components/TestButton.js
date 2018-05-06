f5e429ae6c3be9a391e06c7702bf476fd3f56f3b
chhavi <s.chhavi@iitg.ac.in>
2018-05-06 11:51:40 +0530

    Clean up the TestButton

--- components/TestButton.js
+++ components/TestButton.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
-import {Button} from 'react-bootstrap';
+import {Button, BUttonGroup} from 'react-bootstrap';
 
-/* I think it will need to converted to css first*/
 import '../less/buttons.less';
 import '../less/colors.less';
 
-/* Write some react function here?*/
 
 function TestButton(){
 	return(
-		<button class="btn" type="button">Default button</button>
-			);
+		<div>		
+		<button class="btn" type="button">Hello There!!</button>		
+		</div>		
+		);
 
 }
 
