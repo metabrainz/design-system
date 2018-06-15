e898abec4cfa1df6b4ca66653e9364442333355f
chhavi <s.chhavi@iitg.ac.in>
2018-06-15 09:40:33 +0530

    Add story for alerts

--- stories/index.stories.js
+++ stories/index.stories.js
@@ -1,19 +1,60 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {ButtonGroup} from 'react-bootstrap';
 import {storiesOf} from '@storybook/react';
 import {action} from '@storybook/addon-actions';
 import {linkTo} from '@storybook/addon-links';
 import {withInfo} from '@storybook/addon-info';
 
-import {Badge, Button, Breadcrumb} from '../components';
+import {Alert, Badge, Button, Breadcrumb} from '../components';
+import AlertDismissable from '../components/Alert';
 
 import '../less/styles.less';
 
 // Stories of components
+storiesOf('Alert', module)
+  .add('Danger Alert', withInfo('')(() => (
+    <div>
+      <AlertDismissable bsStyle="danger">
+        <h4> {'Delete this entity'} </h4>
+        <p>{'You are about to delete this entity. Are you sure you are done here?'}</p>
+        <Button bsStyle="danger">{'Yes, Delete'}</Button>
+        <Button onClick={this.handleDismiss}>{'Cancel'}</Button>
+      </AlertDismissable>
+    </div>
+  )))
+
+  .add('Info Alert', withInfo('')(() => (
+    <div>
+      <AlertDismissable bsStyle="info">
+        <h4> {'Hmm, you cannot edit that one'} </h4>
+        <p>{'Permissions to delete this entity are only granted to priviledged editors.'} </p>
+        <Button bsStyle="primary">{'Got it'}</Button>
+        <Button bsStyle="link">{'Know more'}</Button>
+      </AlertDismissable>
+    </div>
+  )))
+
+  .add('Warning Alert', withInfo('')(() => (
+    <div>
+      <AlertDismissable bsStyle="warning">
+        <p>{'We think your browser might be outdated. Better check that one out before proceeding!'}</p>
+      </AlertDismissable>
+    </div>
+  )))
+
+  .add('Success Alert', withInfo('')(() => (
+    <div>
+      <AlertDismissable bsStyle="success">
+        <p>{'Your tag has been successfully added. Do vote on other relevant tags as well!'}</p>
+      </AlertDismissable>
+    </div>
+  )));
+
 storiesOf('Badge', module)
   .add('Default', withInfo('')(() => (
     <div>
-      Open Edits <Badge>42</Badge>
+      {'Open Edits'} <Badge>{'42'}</Badge>
     </div>
   )));
 
