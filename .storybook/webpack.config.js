7e988dcd509b9117577baf426603cf4bceeede9e
Michael Wiencek <mwtuea@gmail.com>
2019-07-06 19:43:57 -0500

    Upgrade storybook, babel

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,17 +1,17 @@
-const path = require("path");
+const path = require('path');
 
-module.exports = {
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        loaders: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
-        include: path.resolve(__dirname, "../")
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-        loader: 'url-loader?limit=100000',
-      },
-    ]
-  }
+module.exports = ({config, mode}) => {
+  config.module.rules.push(
+    {
+      include: path.resolve(__dirname, '../'),
+      loaders: ['style-loader', 'css-loader', 'postcss-loader', 'sass-loader'],
+      test: /\.scss$/,
+    },
+    {
+      loader: 'url-loader?limit=100000',
+      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+    },
+  );
+
+  return config;
 };
