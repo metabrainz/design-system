afef5cdb211f111e94e01c985bc1d5d898e108d6
akshaaatt <tiwariakshat03@gmail.com>
2022-01-23 03:30:33 +0530

    Fix migration to meb takeover

---
+++ components/index.js
@@ -0,0 +1,25 @@
+import AlertDismissable from './shared/AlertDismissable';
+import Badge from './shared/Badge';
+import Breadcrumb from './shared/Breadcrumb';
+import Button from './shared/Button';
+import Dropdown from './shared/Dropdown';
+import DropdownButton from './shared/DropdownButton';
+import Modal from './shared/Modal';
+import Pagination from './shared/Pagination';
+import ProgressBar from './shared/ProgressBar';
+import Table from './shared/Table';
+import Tooltip from './shared/Tooltip';
+
+export {
+  AlertDismissable,
+  Badge,
+  Breadcrumb,
+  Button,
+  Dropdown,
+  DropdownButton,
+  Modal,
+  Pagination,
+  ProgressBar,
+  Table,
+  Tooltip,
+};
