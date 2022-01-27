c1c86bdabdcb8f4a4ffe8410de815cea50f49957
akshaaatt <tiwariakshat03@gmail.com>
2022-01-28 03:29:00 +0530

    Remove popper js

--- components/index.js
+++ components/index.js
@@ -1,3 +1,5 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import AlertDismissable from './shared/AlertDismissable';
 import Badge from './shared/Badge';
 import Breadcrumb from './shared/Breadcrumb';
